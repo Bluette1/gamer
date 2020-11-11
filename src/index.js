257244c11120eeaed3a78dab7ad8d248de0e1961
Bluette1 <marylene.sawyer@gmail.com>
2020-11-11 13:08:29 +0200

    Add `Game` scene
    - modify `gitignore` to include build/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'phaser';
 import config from './Config/config';
-// import GameScene from './Scenes/GameScene';
+import Game from './Scenes/Game';
 import Boot from './Scenes/Boot';
 import Preload from './Scenes/Preload';
 import Begin from './Scenes/Begin';
@@ -8,7 +8,7 @@ import SetUp from './Scenes/SetUp';
 import Credits from './Scenes/Credits';
 import MusicModel from './MusicModel';
 
-class Game extends Phaser.Game {
+class GamePlay extends Phaser.Game {
   constructor() {
     super(config);
     const musicModel = new MusicModel();
@@ -18,9 +18,10 @@ class Game extends Phaser.Game {
     this.scene.add('Begin', Begin);
     this.scene.add('Settings', SetUp);
     this.scene.add('Credits', Credits);
-    // this.scene.add('Game', GameScene);
+    this.scene.add('Game', Game);
     this.scene.start('Boot');
   }
+
 }
 
-window.game = new Game();
+window.game = new GamePlay();
