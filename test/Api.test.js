11f9262acbf664079d1d1d4127ed448e4b540faa
Bluette1 <marylene.sawyer@gmail.com>
2020-11-16 18:39:08 +0200

    Fix eslint errors

--- test/Api.test.js
+++ test/Api.test.js
@@ -1,16 +1,15 @@
-const { default: Api } = require("../src/ScoreService/Api");
-
 const axios = require('axios');
-const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
+const { default: Api } = require('../src/ScoreService/Api');
+
 jest.mock('axios');
 afterEach(() => {
   jest.clearAllMocks();
 });
 
-it('should properly decorate the fullName', async() => {
+it('should properly decorate the fullName', async () => {
   axios.post.mockResolvedValueOnce({
-    data: { result: "GameId" }
+    data: { result: 'GameId' },
   });
   const gameId = await Api.postApiGame('Gamer');
-  expect(gameId).toEqual("GameId");
+  expect(gameId).toEqual('GameId');
 });
