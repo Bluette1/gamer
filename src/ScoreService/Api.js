09972d328e4bed0893798e5df0eed2eace9ab245
Bluette1 <marylene.sawyer@gmail.com>
2020-11-16 07:38:59 +0200

    Fix eslint errors: slight fixes

--- src/ScoreService/Api.js
+++ src/ScoreService/Api.js
@@ -3,19 +3,15 @@ import axios from 'axios';
 const Api = (() => {
   const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 
-  const postApiGame = (name) => {
-    axios.post(`${baseUrl}/games/`, { name })
-      .then((response) => response.data.result)
-      .catch(() => {});
-  };
+  const postApiGame = (name) => axios.post(`${baseUrl}/games/`, { name })
+    .then((response) => response.data.result)
+    .catch(() => {});
 
   const gameId = process.env.GAME_ID || postApiGame('Gamer');
 
-  const getApiScores = (callback) => {
-    axios.get(`${baseUrl}/games/${gameId}/scores/`)
-      .then((response) => callback(null, response.data.result))
-      .catch((error) => callback(error));
-  };
+  const getApiScores = (callback) => axios.get(`${baseUrl}/games/${gameId}/scores/`)
+    .then((response) => callback(null, response.data.result))
+    .catch((error) => callback(error));
 
   const sortApiScores = (scoresArr) => {
     scoresArr.sort((a, b) => b.score - a.score);
