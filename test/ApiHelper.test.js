83a99fdffec0e0f0cb5b7e8571dafe9032a8e424
Bluette1 <marylene.sawyer@gmail.com>
2020-11-16 22:16:29 +0200

    Add missing test cases ApiHelper

--- test/ApiHelper.test.js
+++ test/ApiHelper.test.js
@@ -40,3 +40,33 @@ test('the top scores are returned when array length is  > than count', () => {
   ];
   expect(ApiHelper.topScores(3, resultArray)).toEqual(sortedArray);
 });
+
+test('the top scores are returned without duplicate entries', () => {
+  const sortedArray = [
+    { user: 'John', score: 3500 },
+    { user: 'Anne', score: 2500 },
+    { user: 'Scott', score: 1500 },
+  ];
+  const resultArray = [
+    { user: 'John', score: 3500 },
+    { user: 'Anne', score: 1500 },
+    { user: 'Anne', score: 2500 },
+    { user: 'Scott', score: 1500 },
+    { user: 'John', score: 500 },
+  ];
+  expect(ApiHelper.topScores(3, resultArray)).toEqual(sortedArray);
+});
+
+test('the top scores are returned without invalid entries: negative scores or empty names', () => {
+  const sortedArray = [
+    { user: 'John', score: 3500 },
+    { user: 'Anne', score: 1500 },
+  ];
+  const resultArray = [
+    { user: 'John', score: 3500 },
+    { user: 'Anne', score: 1500 },
+    { user: '', score: 2500 },
+    { user: 'Jane', score: -1 },
+  ];
+  expect(ApiHelper.topScores(3, resultArray)).toEqual(sortedArray);
+});
