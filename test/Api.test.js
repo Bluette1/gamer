f000de555c52cd449078e998cc59c3b72471d45a
Bluette1 <marylene.sawyer@gmail.com>
2020-11-16 19:33:15 +0200

    Add test for the api request to retrieve scores

--- test/Api.test.js
+++ test/Api.test.js
@@ -1,6 +1,28 @@
 const axios = require('axios');
 const { default: Api } = require('../src/ScoreService/Api');
 
+const resultArray = [{
+  user: 'Jane',
+  score: 1000,
+},
+{
+  user: 'Mary',
+  score: 1200,
+},
+{
+  user: 'Scott',
+  score: 1500,
+},
+{
+  user: 'Anne',
+  score: 2500,
+},
+{
+  user: 'John',
+  score: 3500,
+},
+];
+
 jest.mock('axios');
 afterEach(() => {
   jest.clearAllMocks();
@@ -12,4 +34,18 @@ it('should return the game id', async () => {
   });
   const gameId = await Api.postApiGame('Gamer');
   expect(gameId).toEqual('GameId');
+});
+
+it('should return the game scores', async () => {
+  const mockUpdateLeadersSpy = jest.fn(() => {});
+
+  axios.post.mockResolvedValueOnce({
+    data: { result: 'GameId' },
+  });
+
+  axios.get.mockResolvedValueOnce({
+    data: { result: resultArray },
+  });
+  await Api.getApiScores(mockUpdateLeadersSpy);
+  expect(mockUpdateLeadersSpy).toHaveBeenCalledWith(null, resultArray);
 });
